e614e82bc175dd39754de0ec296083cf49d518a5
TDWalsh <thomas.walsh736@gmail.com>
2020-09-08 23:43:58 -0400

    added a scores.html file to be the destination of both the highscores link and the page directed to after the quiz is over. also added a function that populates the page with the first question set upon clicking the start quiz button

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 var startingTime = 60;
-var timerEl = document.getElementById("#timer");
+var questionEL = document.getElementById("questionText");
+var answerOneEl = document.getElementById("btn1");
+var answerTwoEl = document.getElementById("btn2");
+var answerThreeEl = document.getElementById("btn3");
+var answerFourEl = document.getElementById("btn4");
+var quizPageArray = [
+    {
+        "set": "1",
+        "question": "Commonly used data types DO NOT include:",
+        "answer1": "1. strings",
+        "answer2": "2. booleans",
+        "answer3": "3. alerts",
+        "answer4": "4. numbers"
+    },
+    {
+        "set": "2",
+        "question": "The condition in an if/else statement is enclosed within ____.",
+        "answer1": "1. quotes",
+        "answer2": "2. curly brackets",
+        "answer3": "3. parentheses",
+        "answer4": "4. square brackets"
+    },
+    {
+        "set": "3",
+        "question": "Arrays in JavaScript can be used to store ____.",
+        "answer1": "1. numbers and strings",
+        "answer2": "2. other arrays",
+        "answer3": "3. booleans",
+        "answer4": "4. all of the above"
+    },
+    {
+        "set": "4",
+        "question": "String values must be stored within ____ when being assigned to variables.",
+        "answer1": "1. commas",
+        "answer2": "2. curly brackets",
+        "answer3": "3. quotes",
+        "answer4": "4. parentheses"
+    },
+    {
+        "set": "5",
+        "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
+        "answer1": "1. JavaScript",
+        "answer2": "2. terminal/bash",
+        "answer3": "3. for loops",
+        "answer4": "4. console log"
+    }
+];
 
 
 
@@ -12,9 +58,26 @@ document.getElementById("startButton").addEventListener("click", function(){
     function countDown() {
         startingTime--;
         document.getElementById("timer").innerHTML = "Timer: " + startingTime;
-        
-        
+        if(startingTime === 0){
+            alert("GAME OVER");
+        }
     }
-
+    startQuiz();
 });
+
+function startQuiz(){
+    document.getElementById("startButton").remove();
+    document.getElementById("instructions").remove();
+    answerOneEl.style.visibility = "visible";
+    answerTwoEl.style.visibility = "visible";
+    answerThreeEl.style.visibility = "visible";
+    answerFourEl.style.visibility = "visible";
+    questionEL.textContent = quizPageArray[0].question;
+    answerOneEl.textContent = quizPageArray[0].answer1;
+    answerTwoEl.textContent = quizPageArray[0].answer2;
+    answerThreeEl.textContent = quizPageArray[0].answer3;
+    answerFourEl.textContent = quizPageArray[0].answer4;
+
+
+};
 
