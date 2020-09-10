d7bd5d560d03ddd8f5ee0581b95052916ddb97a6
TDWalsh <thomas.walsh736@gmail.com>
2020-09-09 22:17:40 -0400

    now storing final score in the local storage

--- script.js
+++ script.js
@@ -6,107 +6,105 @@ var answerThreeEl = document.getElementById("btn3");
 var answerFourEl = document.getElementById("btn4");
 var pageIndex = 1;
 var answerButtonsEl = document.getElementsByClassName("answerButton");
+var playerName = "";
+var finalScore = 0;
 var quizPageArray = [
-    {
-        "set": "1",
-        "question": "Commonly used data types DO NOT include:",
-        "answer1": "1. strings",
-        "answer2": "2. booleans",
-        "answer3": "3. alerts",
-        "answer4": "4. numbers",
-        "correct": "3. alerts"
-    },
-    {
-        "set": "2",
-        "question": "The condition in an if/else statement is enclosed within ____.",
-        "answer1": "1. quotes",
-        "answer2": "2. curly brackets",
-        "answer3": "3. parentheses",
-        "answer4": "4. square brackets",
-        "correct": "2. curly brackets"
-    },
-    {
-        "set": "3",
-        "question": "Arrays in JavaScript can be used to store ____.",
-        "answer1": "1. numbers and strings",
-        "answer2": "2. other arrays",
-        "answer3": "3. booleans",
-        "answer4": "4. all of the above",
-        "correct": "4. all of the above"
-    },
-    {
-        "set": "4",
-        "question": "String values must be stored within ____ when being assigned to variables.",
-        "answer1": "1. commas",
-        "answer2": "2. curly brackets",
-        "answer3": "3. quotes",
-        "answer4": "4. parentheses",
-        "correct": "3. quotes"
-    },
-    {
-        "set": "5",
-        "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
-        "answer1": "1. JavaScript",
-        "answer2": "2. terminal/bash",
-        "answer3": "3. for loops",
-        "answer4": "4. console log",
-        "correct": "4. console log"
-    }
+  {
+    set: "1",
+    question: "Commonly used data types DO NOT include:",
+    answer1: "1. strings",
+    answer2: "2. booleans",
+    answer3: "3. alerts",
+    answer4: "4. numbers",
+    correct: "3. alerts",
+  },
+  {
+    set: "2",
+    question: "The condition in an if/else statement is enclosed within ____.",
+    answer1: "1. quotes",
+    answer2: "2. curly brackets",
+    answer3: "3. parentheses",
+    answer4: "4. square brackets",
+    correct: "2. curly brackets",
+  },
+  {
+    set: "3",
+    question: "Arrays in JavaScript can be used to store ____.",
+    answer1: "1. numbers and strings",
+    answer2: "2. other arrays",
+    answer3: "3. booleans",
+    answer4: "4. all of the above",
+    correct: "4. all of the above",
+  },
+  {
+    set: "4",
+    question:
+      "String values must be stored within ____ when being assigned to variables.",
+    answer1: "1. commas",
+    answer2: "2. curly brackets",
+    answer3: "3. quotes",
+    answer4: "4. parentheses",
+    correct: "3. quotes",
+  },
+  {
+    set: "5",
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer1: "1. JavaScript",
+    answer2: "2. terminal/bash",
+    answer3: "3. for loops",
+    answer4: "4. console log",
+    correct: "4. console log",
+  },
 ];
 
-document.getElementById("startButton").addEventListener("click", function(){
-    setInterval(countDown, 1000);
-    
-    startQuiz();
+document.getElementById("startButton").addEventListener("click", function () {
+  setInterval(countDown, 1000);
+
+  startQuiz();
 });
 
-for(i = 0; i < answerButtonsEl.length; i++){
-    answerButtonsEl[i].addEventListener("click", nextPage);
+for (i = 0; i < answerButtonsEl.length; i++) {
+  answerButtonsEl[i].addEventListener("click", nextPage);
 }
-
-
 
 function countDown() {
-    startingTime--;
-    document.getElementById("timer").innerHTML = "Timer: " + startingTime;
-    if(startingTime <= 0){
-        location.assign("scores.html");
-    }
+  startingTime--;
+  document.getElementById("timer").innerHTML = "Timer: " + startingTime;
+  if (startingTime <= 0) {
+    window.localStorage.setItem('finalScore', startingTime);
+    location.assign("scores.html");
+  }
 }
 
+function startQuiz() {
+  document.getElementById("startButton").remove();
+  document.getElementById("instructions").remove();
+  answerOneEl.style.visibility = "visible";
+  answerTwoEl.style.visibility = "visible";
+  answerThreeEl.style.visibility = "visible";
+  answerFourEl.style.visibility = "visible";
+  questionEL.textContent = quizPageArray[0].question;
+  answerOneEl.textContent = quizPageArray[0].answer1;
+  answerTwoEl.textContent = quizPageArray[0].answer2;
+  answerThreeEl.textContent = quizPageArray[0].answer3;
+  answerFourEl.textContent = quizPageArray[0].answer4;
+}
+function nextPage(event) {
 
+  if (quizPageArray[pageIndex - 1].correct !== event.target.textContent) {
+    startingTime -= 10;
+  }
 
-function startQuiz(){
-    document.getElementById("startButton").remove();
-    document.getElementById("instructions").remove();
-    answerOneEl.style.visibility = "visible";
-    answerTwoEl.style.visibility = "visible";
-    answerThreeEl.style.visibility = "visible";
-    answerFourEl.style.visibility = "visible";
-    questionEL.textContent = quizPageArray[0].question;
-    answerOneEl.textContent = quizPageArray[0].answer1;
-    answerTwoEl.textContent = quizPageArray[0].answer2;
-    answerThreeEl.textContent = quizPageArray[0].answer3;
-    answerFourEl.textContent = quizPageArray[0].answer4;
-};
-function nextPage(event){
-    console.log(event.target.textContent);
-    console.log(quizPageArray[pageIndex - 1].correct)
-
-    if(quizPageArray[pageIndex - 1].correct !== event.target.textContent){
-        startingTime -= 10;
-    }
-
-    if(pageIndex < 5){
+  if (pageIndex < 5) {
     questionEL.textContent = quizPageArray[pageIndex].question;
     answerOneEl.textContent = quizPageArray[pageIndex].answer1;
     answerTwoEl.textContent = quizPageArray[pageIndex].answer2;
     answerThreeEl.textContent = quizPageArray[pageIndex].answer3;
     answerFourEl.textContent = quizPageArray[pageIndex].answer4;
     pageIndex++;
-    }
-    else {
-        location.assign("scores.html");
-    }
-
-};
+  } else {
+    window.localStorage.setItem('finalScore', startingTime);
+    location.assign("scores.html");
+  }
+}
